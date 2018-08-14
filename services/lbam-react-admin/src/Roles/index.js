28c590643d703bd4c4e3943a287b3011fbafc27f
meepeek <me@meepeek.com>
2018-08-14 18:35:31 +0700

    clean up

--- services/lbam-react-admin/src/Roles/index.js
+++ services/lbam-react-admin/src/Roles/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import {
-    BooleanField,
-    Create,
-    Datagrid,
-    DateField,
-    DateInput,
-    Edit,
-    EditButton,
-    Filter,
-    FormTab,
-    List,
-    LongTextInput,
-    NullableBooleanInput,
-    NumberField,
-    ReferenceManyField,
-    Responsive,
-    TabbedForm,
-    TextField,
-    TextInput,
-    SimpleForm
+  SelectInput,
+  Datagrid,
+  List,
+  TextField,
+  SimpleForm,
+  Create,
+  Edit,
+  TextInput,
+  EditButton,
+  ReferenceField,
+  ReferenceInput
 } from 'react-admin';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Icon from '@material-ui/icons/Person';
@@ -26,11 +18,7 @@ import Icon from '@material-ui/icons/Person';
 const Title = ({ record = {} }) =>
     record ? <div>{record.name}</div> : null;
 
-const listStyles = {
-    nb_commands: { color: 'purple' },
-};
-
-const list = withStyles(listStyles)(({ classes, ...props }) => (
+const list = ({ classes, ...props }) => (
     <List
         {...props}
         // filters={<VisitorFilter />}
@@ -43,44 +31,24 @@ const list = withStyles(listStyles)(({ classes, ...props }) => (
         <EditButton />
       </Datagrid>
     </List>
-));
+);
 
-const editStyles = {
-    first_name: { display: 'inline-block' },
-    last_name: { display: 'inline-block', marginLeft: 32 },
-    email: { width: 544 },
-    address: { maxWidth: 544 },
-    zipcode: { display: 'inline-block' },
-    city: { display: 'inline-block', marginLeft: 32 },
-    comment: {
-        maxWidth: '20em',
-        overflow: 'hidden',
-        textOverflow: 'ellipsis',
-        whiteSpace: 'nowrap',
-    },
-};
-
-const create = withStyles(editStyles)(({ classes, ...props }) => (
+const create = ({ classes, ...props }) => (
     <Create {...props}>
       <SimpleForm>
-          <TextInput
-              source="name"
-              formClassName={classes.name}
-          />
+        <TextInput source="name" />
       </SimpleForm>
     </Create>
-));
+);
 
-const edit = withStyles(editStyles)(({ classes, ...props }) => (
+const edit = ({ classes, ...props }) => (
   <Edit title={<Title />} {...props}>
     <SimpleForm>
-        <TextInput
-            source="name"
-            formClassName={classes.name}
-        />
+      <TextField source="id" />
+      <TextInput source="name" />
     </SimpleForm>
   </Edit>
-));
+);
 
 export default {
   list,
